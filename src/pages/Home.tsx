8c235bcb7ec17def6f606f80a442a6ad7bcdf5a3
Ian Lim <ianester59@gmail.com>
2019-10-04 12:14:59 +0800

    init

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,52 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonList, IonItem } from '@ionic/react';
+import {FC, useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Home: React.FC = () => {
+// newsapi.org API Key
+const API_KEY = 'ac90d2bc71084896a7e4692d843fe305';
+// newsapi.org url
+const URL = `https://newsapi.org/v2/top-headlines?country=ph&apiKey=${API_KEY}`;
+
+// fetch all articles
+const fetchArticles = () => {
+  return axios({
+    url: URL,
+    method: 'get'
+  }).then(response => {
+    console.log(response);
+    return response.data;
+  })
+};
+
+const Home: FC = () => {
+
+  const [ articles, setArticles ] = useState([]);
+  // const items: any[] = [];
+
+  useEffect(() => {
+    fetchArticles().then(data => setArticles(data.articles));
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+          <IonTitle>PH News</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        <IonList>
+          {
+            articles.map((a, index) => {
+              return(
+                <IonItem key={index}>
+                  {a['title']}
+                  <IonButton href={a['url']} color="primary" slot="end">Read</IonButton>
+                </IonItem>
+              )
+            })
+          }
+        </IonList>
       </IonContent>
     </IonPage>
   );
